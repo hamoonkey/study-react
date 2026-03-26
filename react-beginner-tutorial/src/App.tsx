b255cbc559ac868cf2b38a4684693d7137d88d88
前田睦人 <mutsuto.maeda@jhb.muratec.co.jp>
2026-03-26 16:57:28 +0900

    update

--- react-beginner-tutorial/src/App.tsx
+++ react-beginner-tutorial/src/App.tsx
@@ -1,18 +1,71 @@
-import { Button, ChakraProvider } from '@chakra-ui/react'
-import './App.css'
-import { Title } from './Title'
+import {
+  Box,
+  Button,
+  ChakraProvider,
+  Checkbox,
+  Flex,
+  Input,
+  Text,
+} from "@chakra-ui/react";
+import "./App.css";
 
 function App() {
+  const testData = [
+    // 支出と入金のデータを作成
+    {
+      id: 1,
+      title: "お金を払う",
+      isIncome: false,
+      amount: 1000,
+    },
+    {
+      id: 2,
+      title: "お金をもらう",
+      isIncome: true,
+      amount: 1000,
+    },
+  ];
+
+  let title: string = "";
+  let amount: number = 0;
+  let isIncome: boolean = false;
+
   return (
     <ChakraProvider>
       <div>
-        <Title></Title>
-        <Title></Title>
-        <Title></Title>
-        <Button colorScheme="blue">ボタン</Button>
+        <Text fontSize="2x1">家計簿アプリ</Text>
+        <Box mb="8px">
+          <Input
+            placeholder="タイトルを入力"
+            mb="4px"
+            onChange={(e) => (title = e.target.value)}
+          ></Input>
+          <Input
+            placeholder="支出を入力"
+            mb="4px"
+            onChange={(e) => (amount = e.target.value)}
+          ></Input>
+          <Flex align="center" justifyContent="space-between">
+            <Checkbox w="100px">入金</Checkbox>
+            <Button colorScheme="teal">追加</Button>
+          </Flex>
+        </Box>
+        <div>
+          {testData.map((data) => (
+            <div key={data.id}>
+              <Flex align="center" justifyContent="space-between">
+                <Text>{data.title}</Text>
+                <Text>
+                  {data.isIncome ? "+" : "-"}
+                  {data.amount}
+                </Text>
+              </Flex>
+            </div>
+          ))}
+        </div>
       </div>
     </ChakraProvider>
-  )
+  );
 }
 
-export default App
+export default App;
